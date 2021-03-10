bcb5fd18c589d080aab8dc72dc17573973412c31
roman-yagodin <roman.yagodin@gmail.com>
2021-03-10 15:57:46 +0300

    Update plugin info, remove readme

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_volgau do
-  name 'Redmine Volgau plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'Redmine VolGAU'
+  author 'Roman M. Yagodin'
+  description 'Collection of Redmine tweaks for projects.volgau.com'
+  version '0.1.0'
+  url 'https://github.com/volgau/redmine_volgau'
+  author_url 'https://github.com/roman-yagodin'
 end
